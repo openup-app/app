2c6b277b09abaadb93fc592ab55d394b8ac75ff1
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2022-02-15 21:00:57 +1030

    Show serious mode overlay once per app launch

--- lib/menu_screen.dart
+++ lib/menu_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:openup/widgets/button.dart';
 import 'package:openup/widgets/home_button.dart';
@@ -7,6 +10,16 @@ import 'package:openup/widgets/theming.dart';
 import 'package:openup/widgets/toggle_button.dart';
 
 part 'menu_screen.freezed.dart';
+
+final _shouldOverlayProvider =
+    StateNotifierProvider<_ShouldOverlay, bool>((ref) {
+  return _ShouldOverlay();
+});
+
+class _ShouldOverlay extends StateNotifier<bool> {
+  _ShouldOverlay() : super(true);
+  void disableOverlay() => state = false;
+}
 
 class MenuScreen extends StatefulWidget {
   final String label;
@@ -32,6 +45,7 @@ class MenuScreen extends StatefulWidget {
 
 class _MenuScreenState extends State<MenuScreen> {
   bool _serious = false;
+  bool _showSeriousOverlay = false;
 
   @override
   Widget build(BuildContext context) {
@@ -45,140 +59,165 @@ class _MenuScreenState extends State<MenuScreen> {
         ),
       ],
     );
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Colors.white,
-            MenuScreenTheme.of(context).backgroundGradientBottom,
-          ],
-        ),
-      ),
-      child: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 22.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Spacer(),
-                Text(
-                  widget.label,
-                  style: Theming.of(context).text.headline.copyWith(
-                    color: MenuScreenTheme.of(context).titleColor,
-                    shadows: [
-                      BoxShadow(
-                        color: MenuScreenTheme.of(context).titleShadowColor,
-                        spreadRadius: 2.0,
-                        blurRadius: 16.0,
-                        offset: const Offset(0.0, 2.0),
-                      )
-                    ],
-                  ),
-                ),
-                const Spacer(),
-                Stack(
-                  alignment: Alignment.bottomRight,
-                  children: [
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: SizedBox(
-                        height: 185,
-                        child: widget.image,
-                      ),
-                    ),
-                    if (!widget.groupCalling)
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          ToggleButton(
-                            value: _serious,
-                            color: _serious
-                                ? const Color.fromRGBO(0xFF, 0x86, 0x86, 1.0)
-                                : const Color.fromRGBO(0x8B, 0xC0, 0xFF, 1.0),
-                            onChanged: (value) {
-                              setState(() => _serious = value);
-                            },
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            _serious ? 'serious' : 'casual',
-                            style: TextStyle(
-                              color: _serious
-                                  ? const Color.fromRGBO(0xFF, 0x86, 0x86, 1.0)
-                                  : const Color.fromRGBO(0x8B, 0xC0, 0xFF, 1.0),
-                              fontSize: 14,
-                            ),
-                          ),
-                          const SizedBox(height: 8),
-                        ],
-                      ),
-                  ],
-                ),
-                _MenuButton(
-                  onPressed: () => widget.onPressedVoiceCall(_serious),
-                  icon: Image.asset('assets/images/voice_call.png'),
-                  color: MenuScreenTheme.of(context).buttonColorTop,
-                  child: Text(
-                    'Talk to someone new',
-                    style: buttonStyle,
-                  ),
-                ),
-                ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    minHeight: 16,
-                    maxHeight: 28,
-                  ),
-                ),
-                _MenuButton(
-                  onPressed: () => widget.onPressedVideoCall(_serious),
-                  icon: Image.asset('assets/images/video_call.png'),
-                  color: MenuScreenTheme.of(context).buttonColorMiddle,
-                  child: Text(
-                    'Video call someone new',
-                    style: buttonStyle,
-                  ),
-                ),
-                ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    minHeight: 16,
-                    maxHeight: 28,
-                  ),
-                ),
-                _MenuButton(
-                  onPressed: widget.onPressedPreferences,
-                  icon: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Image.asset('assets/images/preferences.png'),
-                  ),
-                  color: MenuScreenTheme.of(context).buttonColorBottom,
-                  child: Text(
-                    'Preferences',
-                    style: buttonStyle,
-                  ),
-                ),
-                const Spacer(),
+    return Stack(
+      children: [
+        DecoratedBox(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.white,
+                MenuScreenTheme.of(context).backgroundGradientBottom,
               ],
             ),
           ),
-          Positioned(
-            top: MediaQuery.of(context).padding.top + 16,
-            right: MediaQuery.of(context).padding.right + 16,
-            child: ProfileButton(
-              color: MenuScreenTheme.of(context).profileButtonColor,
-            ),
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 22.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Spacer(),
+                    Text(
+                      widget.label,
+                      style: Theming.of(context).text.headline.copyWith(
+                        color: MenuScreenTheme.of(context).titleColor,
+                        shadows: [
+                          BoxShadow(
+                            color: MenuScreenTheme.of(context).titleShadowColor,
+                            spreadRadius: 2.0,
+                            blurRadius: 16.0,
+                            offset: const Offset(0.0, 2.0),
+                          )
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: SizedBox(
+                            height: 185,
+                            child: widget.image,
+                          ),
+                        ),
+                        if (!widget.groupCalling)
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Consumer(builder: (context, ref, child) {
+                                return ToggleButton(
+                                  value: _serious,
+                                  color: _serious
+                                      ? const Color.fromRGBO(
+                                          0xFF, 0x86, 0x86, 1.0)
+                                      : const Color.fromRGBO(
+                                          0x8B, 0xC0, 0xFF, 1.0),
+                                  onChanged: (value) {
+                                    final shouldOverlay =
+                                        ref.read(_shouldOverlayProvider);
+                                    ref
+                                        .read(_shouldOverlayProvider.notifier)
+                                        .disableOverlay();
+                                    setState(() {
+                                      _showSeriousOverlay =
+                                          shouldOverlay && value;
+                                      _serious = value;
+                                    });
+                                  },
+                                );
+                              }),
+                              const SizedBox(height: 8),
+                              Text(
+                                _serious ? 'serious' : 'casual',
+                                style: TextStyle(
+                                  color: _serious
+                                      ? const Color.fromRGBO(
+                                          0xFF, 0x86, 0x86, 1.0)
+                                      : const Color.fromRGBO(
+                                          0x8B, 0xC0, 0xFF, 1.0),
+                                  fontSize: 14,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                            ],
+                          ),
+                      ],
+                    ),
+                    _MenuButton(
+                      onPressed: () => widget.onPressedVoiceCall(_serious),
+                      icon: Image.asset('assets/images/voice_call.png'),
+                      color: MenuScreenTheme.of(context).buttonColorTop,
+                      child: Text(
+                        'Talk to someone new',
+                        style: buttonStyle,
+                      ),
+                    ),
+                    ConstrainedBox(
+                      constraints: const BoxConstraints(
+                        minHeight: 16,
+                        maxHeight: 28,
+                      ),
+                    ),
+                    _MenuButton(
+                      onPressed: () => widget.onPressedVideoCall(_serious),
+                      icon: Image.asset('assets/images/video_call.png'),
+                      color: MenuScreenTheme.of(context).buttonColorMiddle,
+                      child: Text(
+                        'Video call someone new',
+                        style: buttonStyle,
+                      ),
+                    ),
+                    ConstrainedBox(
+                      constraints: const BoxConstraints(
+                        minHeight: 16,
+                        maxHeight: 28,
+                      ),
+                    ),
+                    _MenuButton(
+                      onPressed: widget.onPressedPreferences,
+                      icon: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Image.asset('assets/images/preferences.png'),
+                      ),
+                      color: MenuScreenTheme.of(context).buttonColorBottom,
+                      child: Text(
+                        'Preferences',
+                        style: buttonStyle,
+                      ),
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: MediaQuery.of(context).padding.top + 16,
+                right: MediaQuery.of(context).padding.right + 16,
+                child: ProfileButton(
+                  color: MenuScreenTheme.of(context).profileButtonColor,
+                ),
+              ),
+              Positioned(
+                right: MediaQuery.of(context).padding.right + 16,
+                bottom: MediaQuery.of(context).padding.bottom + 16,
+                child: HomeButton(
+                  color: MenuScreenTheme.of(context).homeButtonColor,
+                ),
+              ),
+            ],
           ),
-          Positioned(
-            right: MediaQuery.of(context).padding.right + 16,
-            bottom: MediaQuery.of(context).padding.bottom + 16,
-            child: HomeButton(
-              color: MenuScreenTheme.of(context).homeButtonColor,
-            ),
+        ),
+        if (_showSeriousOverlay)
+          Positioned.fill(
+            child: _SeriousModeOverlay(onClose: () {
+              setState(() => _showSeriousOverlay = false);
+            }),
           ),
-        ],
-      ),
+      ],
     );
   }
 }
@@ -226,6 +265,110 @@ class _MenuButton extends StatelessWidget {
         ),
       ),
       onPressed: onPressed,
+    );
+  }
+}
+
+class _SeriousModeOverlay extends StatelessWidget {
+  final VoidCallback onClose;
+  const _SeriousModeOverlay({
+    Key? key,
+    required this.onClose,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BackdropFilter(
+      filter: ImageFilter.blur(
+        sigmaX: 4,
+        sigmaY: 4,
+      ),
+      child: DecoratedBox(
+        decoration: const BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0.0, 4.0),
+              color: Color.fromRGBO(0x00, 0x00, 0x00, 0.25),
+              blurRadius: 4,
+            ),
+          ],
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromRGBO(0xFD, 0x8C, 0x8C, 1.0),
+              Color.fromRGBO(0xBD, 0x20, 0x20, 0.74),
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).padding.top + 32,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 32.0),
+                child: Button(
+                  onPressed: onClose,
+                  child: Image.asset('assets/images/close_circle.png'),
+                ),
+              ),
+            ),
+            const Spacer(),
+            Center(
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 357),
+                child: Text.rich(
+                  TextSpan(
+                    style: Theming.of(context).text.body.copyWith(
+                        fontSize: 18, fontWeight: FontWeight.w700, height: 1.7),
+                    children: [
+                      const TextSpan(
+                          text:
+                              'Serious about meeting people? We can help you out with that! Toggling this switch '),
+                      WidgetSpan(
+                        child: SizedBox(
+                          width: 23,
+                          height: 28,
+                          child: FittedBox(
+                            fit: BoxFit.scaleDown,
+                            child: IgnorePointer(
+                              child: ToggleButton(
+                                color:
+                                    const Color.fromRGBO(0xFF, 0x86, 0x86, 1.0),
+                                value: true,
+                                useShadow: true,
+                                onChanged: (_) {},
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const TextSpan(
+                          text:
+                              ' will only connect you with others who are serious about connecting too. When toggled, both parties are required to stay for the entire call. If any person leaves early they will be penalized from serious play for five minutes. We do this to help you find someone who is serious about finding others too.'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 54),
+            Padding(
+              padding: const EdgeInsets.only(left: 125),
+              child: Text(
+                '- openup',
+                style: Theming.of(context)
+                    .text
+                    .body
+                    .copyWith(fontSize: 36, fontWeight: FontWeight.w700),
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
     );
   }
 }
