3e6d0bdb92db3148a74c5b8bdd9c326e08e201cb
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2022-03-05 17:13:58 +1030

    Disable back button on first onboarding screen

--- lib/sign_up_info_screen.dart
+++ lib/sign_up_info_screen.dart
@@ -26,66 +26,69 @@ class _SignUpInfoScreenState extends ConsumerState<SignUpInfoScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return KeyboardScreen(
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Who are you?',
-                style: Theming.of(context).text.body.copyWith(
-                      color: const Color.fromRGBO(0x62, 0xCD, 0xE3, 1.0),
-                      fontWeight: FontWeight.w400,
-                      fontSize: 48,
-                    ),
-              ),
-              const SizedBox(height: 28),
-              ConstrainedBox(
-                constraints: const BoxConstraints(maxWidth: 350),
-                child: Text(
-                  'Openup is about building real connections in a real way. Please respond to the following information to help those wanting to find someone like you, find you!',
-                  textAlign: TextAlign.center,
+    return WillPopScope(
+      onWillPop: () => Future.value(false),
+      child: KeyboardScreen(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Who are you?',
                   style: Theming.of(context).text.body.copyWith(
-                        color: const Color.fromRGBO(0x99, 0x99, 0x99, 1.0),
+                        color: const Color.fromRGBO(0x62, 0xCD, 0xE3, 1.0),
                         fontWeight: FontWeight.w400,
-                        fontSize: 18,
+                        fontSize: 48,
                       ),
                 ),
-              ),
-              const SizedBox(height: 24),
-              InputArea(
-                color: const Color.fromRGBO(0xED, 0xED, 0xED, 1.0),
-                child: TextFormField(
-                  textAlign: TextAlign.center,
-                  controller: _nameController,
-                  textCapitalization: TextCapitalization.sentences,
-                  decoration: InputDecoration.collapsed(
-                    hintText: 'Username',
-                    hintStyle: Theming.of(context).text.body.copyWith(
+                const SizedBox(height: 28),
+                ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 350),
+                  child: Text(
+                    'Openup is about building real connections in a real way. Please respond to the following information to help those wanting to find someone like you, find you!',
+                    textAlign: TextAlign.center,
+                    style: Theming.of(context).text.body.copyWith(
+                          color: const Color.fromRGBO(0x99, 0x99, 0x99, 1.0),
+                          fontWeight: FontWeight.w400,
                           fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                          color: const Color.fromRGBO(0x10, 0x10, 0x10, 1.0),
                         ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 24),
-              SignificantButton.blue(
-                onPressed: _submit,
-                child: _uploading
-                    ? const CircularProgressIndicator(color: Colors.white)
-                    : const Text('Continue'),
-              ),
-            ],
-          ),
-          const Align(
-            alignment: Alignment.bottomCenter,
-            child: MaleFemaleConnectionImageApart(),
-          ),
-        ],
+                const SizedBox(height: 24),
+                InputArea(
+                  color: const Color.fromRGBO(0xED, 0xED, 0xED, 1.0),
+                  child: TextFormField(
+                    textAlign: TextAlign.center,
+                    controller: _nameController,
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Username',
+                      hintStyle: Theming.of(context).text.body.copyWith(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: const Color.fromRGBO(0x10, 0x10, 0x10, 1.0),
+                          ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                SignificantButton.blue(
+                  onPressed: _submit,
+                  child: _uploading
+                      ? const CircularProgressIndicator(color: Colors.white)
+                      : const Text('Continue'),
+                ),
+              ],
+            ),
+            const Align(
+              alignment: Alignment.bottomCenter,
+              child: MaleFemaleConnectionImageApart(),
+            ),
+          ],
+        ),
       ),
     );
   }
