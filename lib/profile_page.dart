cb463afb67f5f195584f4efee2410c786b904a3e
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2022-08-23 13:01:59 +0930

    Add tab view

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,13 +1,13 @@
 import 'dart:typed_data';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart' hide Chip;
 import 'package:openup/api/users/profile.dart';
 import 'package:openup/widgets/button.dart';
 import 'package:openup/widgets/common.dart';
+import 'package:openup/widgets/tab_view.dart';
 import 'package:openup/widgets/theming.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   final Profile profile;
 
   const ProfilePage({
@@ -16,34 +16,44 @@ class ProfilePage extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  bool _showEdit = true;
+
+  @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         SizedBox(height: MediaQuery.of(context).padding.top),
         const SizedBox(height: 12),
-        RichText(
-          text: TextSpan(
-            style: Theming.of(context)
-                .text
-                .body
-                .copyWith(fontWeight: FontWeight.w300),
-            children: [
-              TextSpan(
-                text: 'openup ',
-                style: Theming.of(context)
-                    .text
-                    .body
-                    .copyWith(fontWeight: FontWeight.w600),
-              ),
-              const TextSpan(text: 'make new '),
-              TextSpan(
-                text: 'friends',
-                style: Theming.of(context)
-                    .text
-                    .body
-                    .copyWith(fontWeight: FontWeight.w600),
-              ),
-            ],
+        Center(
+          child: RichText(
+            text: TextSpan(
+              style: Theming.of(context)
+                  .text
+                  .body
+                  .copyWith(fontWeight: FontWeight.w300),
+              children: [
+                TextSpan(
+                  text: 'openup ',
+                  style: Theming.of(context)
+                      .text
+                      .body
+                      .copyWith(fontWeight: FontWeight.w600),
+                ),
+                const TextSpan(text: 'make new '),
+                TextSpan(
+                  text: 'friends',
+                  style: Theming.of(context)
+                      .text
+                      .body
+                      .copyWith(fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
           ),
         ),
         SizedBox(
@@ -51,25 +61,16 @@ class ProfilePage extends StatelessWidget {
           child: Stack(
             alignment: Alignment.center,
             children: [
-              Align(
-                child: CupertinoSlidingSegmentedControl(
-                  children: {
-                    'edit': Text(
-                      'Edit Profile',
-                      style: Theming.of(context).text.body,
-                    ),
-                    'preview': Text(
-                      'Preview',
-                      style: Theming.of(context).text.body,
-                    ),
-                  },
-                  onValueChanged: (value) {},
-                ),
+              TabView(
+                firstSelected: _showEdit,
+                firstLabel: 'Edit Profile',
+                secondLabel: 'Preview',
+                onSelected: (first) => setState(() => _showEdit = first),
               ),
               Positioned(
                 top: 0,
                 bottom: 0,
-                right: 0,
+                right: 16,
                 child: Button(
                   child: const Padding(
                     padding: EdgeInsets.all(12.0),
@@ -85,33 +86,71 @@ class ProfilePage extends StatelessWidget {
             ],
           ),
         ),
-        Expanded(
-          child: ListView(
-            padding:
-                EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
+        if (_showEdit)
+          Expanded(
+            child: _EditProfileView(
+              profile: widget.profile,
+            ),
+          )
+        else
+          Expanded(
+            child: _ProfileView(
+              profile: widget.profile,
+            ),
+          )
+      ],
+    );
+  }
+}
+
+class _EditProfileView extends StatelessWidget {
+  final Profile profile;
+  const _EditProfileView({
+    Key? key,
+    required this.profile,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Text(
+            'My Pictures',
+            style: Theming.of(context).text.body.copyWith(fontSize: 24),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Text(
+            'Add your best three pictures',
+            style: Theming.of(context)
+                .text
+                .body
+                .copyWith(fontSize: 16, fontWeight: FontWeight.w300),
+          ),
+        ),
+        const SizedBox(height: 6),
+        Container(
+          height: 298,
+          margin: const EdgeInsets.only(left: 16, right: 16),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                child: Text(
-                  'My Pictures',
-                  style: Theming.of(context).text.body.copyWith(fontSize: 24),
+              Expanded(
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(24)),
+                  child: _PhotoOrUploadButton(
+                    url: profile.photo,
+                    onUpload: (_) {},
+                  ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                child: Text(
-                  'Add your best three pictures',
-                  style: Theming.of(context)
-                      .text
-                      .body
-                      .copyWith(fontSize: 16, fontWeight: FontWeight.w300),
-                ),
-              ),
-              const SizedBox(height: 6),
-              Container(
-                height: 298,
-                margin: const EdgeInsets.only(left: 16, right: 16),
-                child: Row(
+              const SizedBox(width: 8),
+              Expanded(
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
                     Expanded(
@@ -119,140 +158,137 @@ class ProfilePage extends StatelessWidget {
                         borderRadius:
                             const BorderRadius.all(Radius.circular(24)),
                         child: _PhotoOrUploadButton(
-                          url: profile.photo,
+                          url: profile.gallery.length > 1
+                              ? profile.gallery[1]
+                              : null,
+                          onUpload: (data) {},
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Expanded(
+                      child: ClipRRect(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(24)),
+                        child: _PhotoOrUploadButton(
+                          url: profile.gallery.length > 2
+                              ? profile.gallery[2]
+                              : null,
                           onUpload: (_) {},
                         ),
                       ),
                     ),
-                    const SizedBox(width: 8),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Expanded(
-                            child: ClipRRect(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(24)),
-                              child: _PhotoOrUploadButton(
-                                url: profile.gallery.length > 1
-                                    ? profile.gallery[1]
-                                    : null,
-                                onUpload: (data) {},
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 8),
-                          Expanded(
-                            child: ClipRRect(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(24)),
-                              child: _PhotoOrUploadButton(
-                                url: profile.gallery.length > 2
-                                    ? profile.gallery[2]
-                                    : null,
-                                onUpload: (_) {},
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
                   ],
                 ),
               ),
-              const SizedBox(height: 29),
-              const Padding(
-                padding: EdgeInsets.only(left: 16, right: 16),
-                child: _RecordButton(),
-              ),
-              const SizedBox(height: 16),
-              Padding(
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                child: Text(
-                  'Reason you\'re here',
-                  style: Theming.of(context).text.body.copyWith(fontSize: 24),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                child: Text(
-                  'Choose a category that fits with you',
-                  style: Theming.of(context)
-                      .text
-                      .body
-                      .copyWith(fontSize: 16, fontWeight: FontWeight.w300),
-                ),
-              ),
-              const SizedBox(height: 6),
-              SingleChildScrollView(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Chip(
-                      label: 'Sad',
-                      selected: false,
-                      onSelected: () {},
-                    ),
-                    Chip(
-                      label: 'Lonely',
-                      selected: false,
-                      onSelected: () {},
-                    ),
-                    Chip(
-                      label: 'Introvert',
-                      selected: false,
-                      onSelected: () {},
-                    ),
-                    Chip(
-                      label: 'Talk',
-                      selected: true,
-                      onSelected: () {},
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 16),
-              Padding(
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                child: Text(
-                  'My Name',
-                  style: Theming.of(context).text.body.copyWith(fontSize: 24),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                child: Text(
-                  'Please don\'t use your real name',
-                  style: Theming.of(context)
-                      .text
-                      .body
-                      .copyWith(fontSize: 16, fontWeight: FontWeight.w300),
-                ),
-              ),
-              const SizedBox(height: 6),
-              Container(
-                height: 51,
-                margin: const EdgeInsets.only(left: 16, right: 16),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white),
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(40),
-                  ),
-                ),
-                child: Center(
-                  child: Text(profile.name,
-                      style: Theming.of(context)
-                          .text
-                          .body
-                          .copyWith(fontSize: 20, fontWeight: FontWeight.w300)),
-                ),
-              ),
-              const SizedBox(height: 16),
             ],
           ),
         ),
+        const SizedBox(height: 29),
+        const Padding(
+          padding: EdgeInsets.only(left: 16, right: 16),
+          child: _RecordButton(),
+        ),
+        const SizedBox(height: 16),
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Text(
+            'Reason you\'re here',
+            style: Theming.of(context).text.body.copyWith(fontSize: 24),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Text(
+            'Choose a category that fits with you',
+            style: Theming.of(context)
+                .text
+                .body
+                .copyWith(fontSize: 16, fontWeight: FontWeight.w300),
+          ),
+        ),
+        const SizedBox(height: 6),
+        SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              Chip(
+                label: 'Sad',
+                selected: false,
+                onSelected: () {},
+              ),
+              Chip(
+                label: 'Lonely',
+                selected: false,
+                onSelected: () {},
+              ),
+              Chip(
+                label: 'Introvert',
+                selected: false,
+                onSelected: () {},
+              ),
+              Chip(
+                label: 'Talk',
+                selected: true,
+                onSelected: () {},
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 16),
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Text(
+            'My Name',
+            style: Theming.of(context).text.body.copyWith(fontSize: 24),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Text(
+            'Please don\'t use your real name',
+            style: Theming.of(context)
+                .text
+                .body
+                .copyWith(fontSize: 16, fontWeight: FontWeight.w300),
+          ),
+        ),
+        const SizedBox(height: 6),
+        Container(
+          height: 51,
+          margin: const EdgeInsets.only(left: 16, right: 16),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(40),
+            ),
+          ),
+          child: Center(
+            child: Text(profile.name,
+                style: Theming.of(context)
+                    .text
+                    .body
+                    .copyWith(fontSize: 20, fontWeight: FontWeight.w300)),
+          ),
+        ),
+        const SizedBox(height: 16),
       ],
+    );
+  }
+}
+
+class _ProfileView extends StatelessWidget {
+  final Profile profile;
+  const _ProfileView({
+    Key? key,
+    required this.profile,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: FlutterLogo(),
     );
   }
 }
---
+++ lib/widgets/tab_view.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+import 'package:openup/widgets/button.dart';
+import 'package:openup/widgets/theming.dart';
+
+class TabView extends StatefulWidget {
+  final bool firstSelected;
+  final String firstLabel;
+  final String secondLabel;
+  final void Function(bool first) onSelected;
+
+  const TabView(
+      {Key? key,
+      required this.firstSelected,
+      required this.firstLabel,
+      required this.secondLabel,
+      required this.onSelected})
+      : super(key: key);
+
+  @override
+  State<TabView> createState() => _TabViewState();
+}
+
+class _TabViewState extends State<TabView> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 36,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          AnimatedPadding(
+            padding: widget.firstSelected
+                ? const EdgeInsets.only(right: 108)
+                : const EdgeInsets.only(left: 108),
+            duration: const Duration(milliseconds: 150),
+            curve: Curves.bounceOut,
+            child: Container(
+              width: 108,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+            ),
+          ),
+          Container(
+            width: 108,
+            height: 48,
+            margin: const EdgeInsets.only(right: 108),
+            child: Button(
+              onPressed: () => widget.onSelected(true),
+              child: Center(
+                child: Text(
+                  widget.firstLabel,
+                  style: Theming.of(context).text.body.copyWith(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                      color:
+                          widget.firstSelected ? Colors.black : Colors.white),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 108,
+            height: 48,
+            margin: const EdgeInsets.only(left: 108),
+            child: Button(
+              onPressed: () => widget.onSelected(false),
+              child: Center(
+                child: Text(
+                  widget.secondLabel,
+                  style: Theming.of(context).text.body.copyWith(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                      color:
+                          !widget.firstSelected ? Colors.black : Colors.white),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
