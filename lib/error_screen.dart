81b26dc6f78f08dc4fa26a41faa772fefe139d6b
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2022-09-11 01:38:48 +0930

    Fix error screen

--- lib/error_screen.dart
+++ lib/error_screen.dart
@@ -11,35 +11,37 @@ class ErrorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Icon(
-            Icons.error,
-            color: Colors.red,
-          ),
-          const SizedBox(height: 16),
-          Text(
-            'Could not connect to server',
-            style: Theming.of(context)
-                .text
-                .subheading
-                .copyWith(color: Colors.black),
-          ),
-          const SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(
-                '/',
-                arguments: InitialLoadingScreenArguments(
-                  needsOnboarding: needsOnboarding,
-                ),
-              );
-            },
-            child: const Text('Retry'),
-          ),
-        ],
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.error,
+              color: Colors.red,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Could not connect to server',
+              style: Theming.of(context)
+                  .text
+                  .subheading
+                  .copyWith(color: Colors.black),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  '/',
+                  arguments: InitialLoadingScreenArguments(
+                    needsOnboarding: needsOnboarding,
+                  ),
+                );
+              },
+              child: const Text('Retry'),
+            ),
+          ],
+        ),
       ),
     );
   }
