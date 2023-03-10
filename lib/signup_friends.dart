bac53dd8ac0429fef2261392dab661bbafd3e07b
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2023-03-10 14:13:32 +1030

    Fix signup friends background

--- lib/signup_friends.dart
+++ lib/signup_friends.dart
@@ -17,79 +17,89 @@ class _SignUpFriendsState extends State<SignUpFriends> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DecoratedBox(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/signup_background.png'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Stack(
-          children: [
-            InviteFriends(
-              padding: EdgeInsets.only(
-                top: 72 + MediaQuery.of(context).padding.top,
-                bottom: MediaQuery.of(context).padding.bottom,
-              ),
-            ),
-            Container(
-              height: 72 + MediaQuery.of(context).padding.top,
-              color: Colors.black.withOpacity(0.2),
-              child: ClipRect(
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 13, sigmaY: 13),
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                        top: MediaQuery.of(context).padding.top + 24),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          const Align(
-                            alignment: Alignment.centerLeft,
-                            child: BackIconButton(),
-                          ),
-                          Text(
-                            'Add friends',
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(
-                                    fontSize: 16, fontWeight: FontWeight.w400),
-                          ),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Button(
-                              onPressed: () {
-                                context.goNamed(
-                                  'discover',
-                                  queryParams: {'welcome': 'true'},
-                                );
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Next',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyMedium!
-                                      .copyWith(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w400),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+      backgroundColor: const Color.fromRGBO(0x1E, 0x1E, 0x1E, 1.0),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaX: 125, sigmaY: 125),
+              child: const Opacity(
+                opacity: 0.25,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                          'assets/images/signup_friends_background.png'),
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          InviteFriends(
+            padding: EdgeInsets.only(
+              top: 72 + MediaQuery.of(context).padding.top,
+              bottom: MediaQuery.of(context).padding.bottom,
+            ),
+          ),
+          Container(
+            height: 72 + MediaQuery.of(context).padding.top,
+            color: Colors.black.withOpacity(0.2),
+            child: ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 13, sigmaY: 13),
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).padding.top + 24),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: BackIconButton(),
+                        ),
+                        Text(
+                          'Add friends',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                  fontSize: 16, fontWeight: FontWeight.w400),
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Button(
+                            onPressed: () {
+                              context.goNamed(
+                                'discover',
+                                queryParams: {'welcome': 'true'},
+                              );
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'Next',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w400),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
