1c6c8febc6124c10aac0380d739a329a4f18ce64
tarloksingh <tarloksinghfilms@gmail.com>
2023-05-06 13:12:25 -0500

    Update signup_background.dart

--- lib/widgets/signup_background.dart
+++ lib/widgets/signup_background.dart
@@ -9,84 +9,100 @@ class SignupBackground extends StatefulWidget {
   final Widget child;
 
   const SignupBackground({
-    super.key,
+    Key? key,
     required this.child,
-  });
+  }) : super(key: key);
 
   @override
   State<SignupBackground> createState() => _SignupBackgroundState();
 }
 
 class _SignupBackgroundState extends State<SignupBackground> {
-  static const _count = 7;
-  final _alignments = List.generate(_count, (index) => Alignment.center);
-  late final List<Timer> _timers;
-
-  @override
-  void initState() {
-    super.initState();
-
-    final r = Random();
-    // Set all elements to a random position from -1 to 1 in x and y
-    for (var i = 0; i < _count; i++) {
-      final a = Alignment(r.nextDouble() * 2 - 1, r.nextDouble() * 2 - 1);
-      setState(() => _alignments[i] = a);
-    }
-
-    _timers = List.generate(_count, (i) {
-      // Randomly change direction every 1 to 2 seconds
-      final time = const Duration(seconds: 1) +
-          Duration(milliseconds: (1000 * r.nextDouble()).toInt());
-      return Timer.periodic(
-        time,
-        (_) {
-          // Set a new random target position for all elements
-          final a = Alignment(r.nextDouble() * 2 - 1, r.nextDouble() * 2 - 1);
-          setState(() => _alignments[i] = a);
-        },
-      );
-    });
-  }
-
-  @override
-  void dispose() {
-    _timers.forEach((timer) => timer.cancel());
-    super.dispose();
-  }
+  final bokehCount = 40;
+  final bokehBaseSize = 40.0;
+  final bokehBaseOpacity = 20.0;
+  final bokehBaseBrightness = 100.0;
+  final bokehBaseDuration = Duration(seconds: 16);
+  final colorSet = [
+    Color(0xff0361a4),
+    Color(0xff018e6d),
+    Color(0xff900005),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    const blur = 100.0;
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        ImageFiltered(
-          imageFilter: ImageFilter.blur(
-            sigmaX: blur,
-            sigmaY: blur,
-            tileMode: TileMode.mirror,
-          ),
-          child: Stack(
-            children: [
-              const Positioned.fill(
-                child: ColoredBox(
-                  color: Colors.black,
-                ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          AnimatedContainer(
+            duration: Duration(seconds: 20),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xff7b0420),
+                  Color(0xff045471),
+                  Color(0xff0262a3),
+                ],
+                stops: [0.0, 0.5, 1.0],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                transform: GradientRotation(-pi / 4),
               ),
-              for (var i = 0; i < _count; i++)
-                AnimatedAlign(
-                  duration: const Duration(milliseconds: 5000),
-                  curve: Curves.easeOut,
-                  alignment: _alignments[i],
-                  child: SvgPicture.asset(
-                    'assets/images/signup_background/$i.svg',
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xff006f8e),
+                  Color(0xff8e000e),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+            child: OverflowBox(
+              maxWidth: double.infinity,
+              maxHeight: double.infinity,
+              child: Stack(
+                children: List.generate(
+                  bokehCount,
+                  (index) => Positioned(
+                    left: Random().nextDouble() * 100,
+                    top: Random().nextDouble() * 100,
+                    width: Random().nextDouble() * bokehBaseSize,
+                    height: Random().nextDouble() * bokehBaseSize,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(1),
+                        color: colorSet[Random().nextInt(colorSet.length)],
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            blurRadius: Random().nextDouble() * 10 + 5,
+                            spreadRadius: Random().nextDouble() * 2,
+                          )
+                        ],
+                        filter: ColorFilter.mode(
+                          Colors.white.withOpacity(
+                            bokehBaseOpacity / 100 +
+                                Random().nextDouble() * 0.8 * 0.15,
+                          ),
+                          BlendMode.modulate,
+                        ),
+                      ),
+                      child: RotationTransition(
+                        turns: AlwaysStoppedAnimation(1),
+                        child: SizedBox.expand(),
+                      ),
+                    ),
                   ),
                 ),
-            ],
+              ),
+            ),
           ),
-        ),
-        widget.child,
-      ],
+        ],
+      ),
     );
   }
 }
